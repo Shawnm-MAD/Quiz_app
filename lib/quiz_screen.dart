486171d5bcffe8f8ad51ec8bf4ed2b07c1d37780
Shawnm-MAD <shawnmathew14@outlook.com>
2025-04-14 14:53:02 -0400

    Initialize quiz state and fetch questions on startup

--- lib/quiz_screen.dart
+++ lib/quiz_screen.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 import 'question.dart';
-import 'api_service.dart';
+import 'api_services.dart';
 
 class QuizScreen extends StatefulWidget {
   @override
   _QuizScreenState createState() => _QuizScreenState();
 }
+class _QuizScreenState extends State<QuizScreen> {
+  List<Question> _questions = [];
+  int _currentQuestionIndex = 0;
+  int _score = 0;
+  bool _loading = true;
+  bool _answered = false;
+  String _selectedAnswer = "";
+  String _feedbackText = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _loadQuestions(); // Fetch questions when screen loads
+  }
